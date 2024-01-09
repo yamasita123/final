8574151fb012ff6124cde4b37f5feaf8ebf5483b
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-09 11:01:29 +0900

    Create db-connect.php

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql220.phy.lolipop.lan';
+    const DBNAME = 'LAA1517480-final';
+    const USER = 'LAA1517480';
+    const PASS = 'final1';
+
+    $connect = 'mysql:host'. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
