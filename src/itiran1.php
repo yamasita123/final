d7d720ef840560cc0ad26949de348281ea878d9f
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-10 14:59:50 +0900

    Update itiran1.php

--- src/itiran1.php
+++ src/itiran1.php
@@ -1,11 +1,11 @@
 <?php if(!empty($_SESSION['final'])){
     echo '<table>';
     echo '<tr><th>番号</th><th>内容</th><th>日付</th></total>';
-    foreach($_SESSION['final'] as $id=>$final){
+    foreach($_SESSION['Todo'] as $id=>$Todo){
         echo '<tr>';
-        echo '<td>', $final['id'], '</td>';
-        echo '<td>',$final['naiyou'], '</td>';
-        echo '<td>',$final['date'], '</td>';
+        echo '<td>', $Todo['id'], '</td>';
+        echo '<td>',$Todo['naiyou'], '</td>';
+        echo '<td>',$Todo['date'], '</td>';
         echo '</tr>';
     }
 }else{
