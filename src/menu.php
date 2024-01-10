e0e3764f718cf2046473141d06948f85cdc729b5
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-10 12:12:27 +0900

    Create menu.php

---
+++ src/menu.php
@@ -0,0 +1,6 @@
+<H1>Todoリスト</H1>
+        <a href="itiran-show.php">一覧</a>
+        <a href="insert.php">新規登録</a>
+        <a href="koushin.php">更新</a>
+        <a href="delete.php">削除</a>
+        <hr>
