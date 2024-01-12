066b8369971a4b446781945963792d3c7a9a2f39
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-12 14:39:00 +0900

    Update insert.php

--- src/insert.php
+++ src/insert.php
@@ -1,13 +1,16 @@
 <?php session_start(); ?>
 <?php require 'menu.php'; ?>
 <?php require 'header.php'; ?>
-<?php 
-    echo '<form action="insert-output.php" method="post">';
-        echo '<table>';
-        echo '<tr><td>内容　：</td><td>';
-        echo '<input type="text" name="naiyou">';
-        echo '</td></tr>';
-        echo '</table>';
-        echo '<input type="submit" value="確定">';
-        echo '</from>';
+<?php
+if(isset($_SESSION['Todo'])){
+    $naiyou=$_SESSION['Todo']['naiyou'];
+}
+echo '<form action="insert-output.php" method="post">';
+echo '<table>';
+echo '<tr><td>内容：</td><td>';
+echo '<input type="text" name="naiyou" value="">';
+echo '</td></tr>';
+echo '</table>';
+echo '<input type="submit" value="登録">';
+echo '</from>';
 ?>
