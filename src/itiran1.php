aafb511a0f0e7ef62365eae57b8ed434ce52c665
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-15 14:56:55 +0900

    Update itiran1.php

--- src/itiran1.php
+++ src/itiran1.php
@@ -1,20 +1,15 @@
-<?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
-
 <?php 
 echo '<table>';
-echo '<tr><th>商品番号</th><th>商品名</th><th>価格</th></tr>'; 
+echo '<tr><th>番号</th><th>内容</th><th>日付</th></tr>'; 
 $pdo=new PDO($connect, USER, PASS);
 $sql=$pdo->query('select * from Todo');
 foreach($sql as $row){
     $id=$row['id'];
     echo '<tr>';
     echo '<td>', $id, '</td>';
-    echo '<td>';
-    echo '<td>', $row['naiyou'], '</td>';
-    echo '</td>';
-    echo '<td>', $date, '</td>';
+    echo '<td>', $row['naiyou'],'</td>';
+    echo '<td>', $row['date'], '</td>';
     echo '</tr>';
 }
 echo '</table>';
