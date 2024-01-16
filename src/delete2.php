68d272a8ac278f0090e1a54af0a1b9614701786e
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-16 14:58:10 +0900

    Update delete2.php

--- src/delete2.php
+++ src/delete2.php
@@ -1,8 +1,29 @@
-<?php session_start(); ?>
+<?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php
-unset($_SESSION['Todo'][$_GET['id']]);
-echo '内容を削除しました。';
-echo '<hr>';
-require 'delete.php';
+$pdo=new PDO($connect, USER, PASS);
+$sql=$pdo->prepare('delete from Todo where id=?');
+if($sql->execute([$_GET['id']])){
+    echo '削除しました。';
+}else{
+    echo '削除できませんでした。';
+}
 ?>
+
+<br><hr><br>
+        <table>
+            <tr><th>番号</th><th>内容</th><th>日付</th></tr>
+            <?php
+                foreach($pdo->query('select * from Todo') as $row){
+                    echo '<tr>';
+                    echo '<td>',$row['id'],'</td>';
+                    echo '<td>',$row['naiyou'],'</td>';
+                    echo '<td>',$row['date'],'</td>';
+                    echo '</tr>';
+                    echo "\n";
+                }
+            ?>
+        </table>
+        <form action="delete.php" method="post">
+            <button type="submit">前へ戻る</button>
+        </form>
