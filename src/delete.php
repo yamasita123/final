8cc87187fe8b3cfda81f498a66950660c4a81a61
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-16 13:30:49 +0900

    Update delete.php

--- src/delete.php
+++ src/delete.php
@@ -1,20 +1,15 @@
-<?php session_start(); ?>
-<?php require 'db-connect.php'; ?>
-<?php require 'header.php'; ?>
-<?php require 'menu.php'; ?>
-<?php 
-echo '<table>';
-echo '<tr><th>番号</th><th>内容</th><th>日付</th></tr>'; 
-$pdo=new PDO($connect, USER, PASS);
-$sql=$pdo->query('select * from Todo');
-foreach($sql as $row){
-    $id=$row['id'];
-    echo '<tr>';
-    echo '<td>', $id, '</td>';
-    echo '<td>', $row['naiyou'],'</td>';
-    echo '<td>', $row['date'], '</td>';
-    echo '<td><a href="delete2.php?id=', $id, '">削除</a></td>';
-    echo '</tr>';
+<?php
+if (!empty($_SESSION['Todo'])) {
+    echo '<table>';
+    echo '<tr><th>番号</th><th>内容</th><th>日付</th></tr>';
+    foreach ($_SESSION['Todo'] as $id=>$Todo){
+        echo '<tr>';
+        echo '<td>', $id, '</td>';
+        echo '<td>', $Todo['naiyou'];
+        echo '<td>', $Todo['date'], '</td>';
+        echo '<td><a href="delete2.php?id=', $id, '">削除</a></td>';
+        echo '</tr>';
+    }
+    echo '</table>';
 }
-echo '</table>';
 ?>
