759949e9c39273dc40fbc04a8b1c7f6b985e843d
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-16 11:16:39 +0900

    Update delete2.php

--- src/delete2.php
+++ src/delete2.php
@@ -6,5 +6,7 @@ $id =  $_POST['id'];
 $sql = $pdo->prepare('DELETE FROM Todo WHERE `Todo`.`id` = $id');
 $sql->bindParam(':id', $id, PDO::PARAM_INT);
 $res = $sql->execute();
+echo "削除されました。"
 require 'itiran1.php';
 ?>
+5と6がエラー
