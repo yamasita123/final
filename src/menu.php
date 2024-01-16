53c7becd95d8a5b4b38146351a1524e598506a6e
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-16 14:58:32 +0900

    Update menu.php

--- src/menu.php
+++ src/menu.php
@@ -2,5 +2,5 @@
         <a href="itiran-show.php">一覧</a>
         <a href="insert.php">新規登録</a>
         <a href="koushin.php">更新</a>
-        <a href="delete-show.php">削除</a>
+        <a href="delete.php">削除</a>
         <hr>
