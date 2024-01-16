9dcff7fcc41adfb61c672af2724df570352e3d5f
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-16 13:30:52 +0900

    Update delete2.php

--- src/delete2.php
+++ src/delete2.php
@@ -1,12 +1,8 @@
 <?php session_start(); ?>
-<?php require 'db-connect.php'; ?>
 <?php require 'header.php'; ?>
 <?php
-$id =  $_POST['id'];
-$sql = $pdo->prepare('DELETE FROM Todo WHERE `Todo`.`id` = $id');
-$sql->bindParam(':id', $id, PDO::PARAM_INT);
-$res = $sql->execute();
-echo "削除されました。"
-require 'itiran1.php';
+unset($_SESSION['Todo'][$_GET['id']]);
+echo '内容を削除しました。';
+echo '<hr>';
+require 'delete.php';
 ?>
-5と6がエラー
