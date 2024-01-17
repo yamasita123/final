aa33fe68af349913902d556f5f23f89409a04246
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-17 11:02:24 +0900

    Update insert.php

--- src/insert.php
+++ src/insert.php
@@ -1,9 +1,12 @@
 <?php require 'menu.php'; ?>
 <?php require 'header.php'; ?>
+<link rel="stylesheet" href="./css/insert.css">
+<div class="insert">
     <form action="insert-output.php" method="post">
         内容：<input type="text" name="naiyou"><br>
         入力した日付：<?php echo date("Y/m/d H:i:s"); ?><br>
         <button type="submit">追加</button>
     </form>
+    </div>
 </body>
 </html>
