7bd852ae6506ce88abc1160c6ed6ebcef237ff55
yamasita123 <2201209@s.asojuku.ac.jp>
2024-01-18 22:35:22 +0900

    Update koushin.php

--- src/koushin.php
+++ src/koushin.php
@@ -1,23 +1,25 @@
 <?php require 'db-connect.php'; ?>
 <?php require 'menu.php'; ?>
 <?php require 'header.php'; ?>
-<link rel="stylesheet" href="./css/koushin.css">
 <div class="koushin">
-
-<?php $pdo=new PDO($connect, USER, PASS);
-
-foreach($pdo->query('select * from Todo') as $row){
-    echo '<form action="koushin-output.php" method="post">';
-    echo '<input type="hidden" name="id" value="', $row['id'], '">';
-    echo '<div class="td0">', $row['id'], '</div>';
-    echo '<div class="td1">';
-	echo '<input type="text" name="naiyou" value="', $row['naiyou'],'">';
-	echo '</div> ';
-    echo '<div class="td2"><input type="submit" value="更新"></div>';
-    echo '</form>';
-    echo "\n";
-}
+<link rel="stylesheet" href="./css/koushin.css">
+        <div class="th0">番号</div>
+		<div class="th1">内容</div>
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+	foreach ($pdo->query('select * from Todo') as $row) {
+		echo '<form action="koushin-output.php" method="post">';
+		echo '<input type="hidden" name="id" value="', $row['id'], '">';
+		echo '<div class="td0">', $row['id'], '</div>';
+		echo '<div class="td1">';
+		echo '<input type="text" name="naiyou" value="', $row['naiyou'],'">';
+		echo '</div> ';
+		echo '<div class="td2"><input type="submit" value="更新"></div>';
+		echo '</form>';
+		echo "\n";
+	}
 ?>
+
 </div>
-</body>
+    </body>
 </html>
